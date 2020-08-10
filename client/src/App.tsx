151697aeb06a441f9e03d59f42360798f9451388
caldric <cvaldric@gmail.com>
2020-08-10 10:43:47 -0400

    Implement routing in React app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from 'react-router-dom';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Switch>
+        <Route exact path="/">
+          <p>This is the index route.</p>
+        </Route>
+        <Route path="/test">
+          <p>This is the test route.</p>
+        </Route>
+      </Switch>
     </div>
   );
-}
+};
 
 export default App;
