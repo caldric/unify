9be265f008410e2bce1d8c4ba0e438d6a07db932
caldric <cvaldric@gmail.com>
2020-08-13 13:06:46 -0400

    Attach state to form text area

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
@@ -9,15 +9,19 @@ const Home: React.FC = () => {
     },
   };
 
+  const [groceries, setGroceries] = useState('');
+
   return (
     <div className="container" style={styles.topComponent}>
       <h2>Groceries</h2>
       <Form>
-        <Form.Group controlId="GroceryListTextarea">
+        <Form.Group controlId="grocery-list">
           <Form.Control
             as="textarea"
             rows={10}
             placeholder="Enter or paste your grocery list items here"
+            value={groceries}
+            onChange={(event) => setGroceries(event.currentTarget.value)}
           />
         </Form.Group>
         <Button variant="primary" type="submit">
