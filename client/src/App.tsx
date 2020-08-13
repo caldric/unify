020f6430df21468cd47ce4141497b8b3d2fa920c
caldric <cvaldric@gmail.com>
2020-08-13 12:27:17 -0400

    Add Bootstrap to client dependencies

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App: React.FC = () => {
   return (
     <div>
       <Switch>
         <Route exact path="/">
-          <p>This is the index route.</p>
+          <h2>This is the index route.</h2>
         </Route>
         <Route path="/test">
-          <p>This is the test route.</p>
+          <h2>This is the test route.</h2>
         </Route>
       </Switch>
     </div>
