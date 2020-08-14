6e4584f327db4eb4f80c551073d3f36ca6ad3879
caldric <cvaldric@gmail.com>
2020-08-14 14:20:23 -0400

    Separate grocery input into its own component

---
+++ client/src/components/GroceryInput.tsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import axios from 'axios';
+
+interface Props {
+  setGroceryOutput: React.Dispatch<React.SetStateAction<string[]>>;
+}
+
+interface ResponseData {
+  message: string;
+  input: string;
+  output: string[];
+}
+
+const GroceryInput: React.FC<Props> = ({ setGroceryOutput }) => {
+  const [groceryInput, setGroceryInput] = useState('');
+
+  const handleSubmit = async (
+    event: React.FormEvent<HTMLElement>
+  ): Promise<void> => {
+    // Prevent page refresh
+    event.preventDefault();
+
+    console.log(`Form submitted with value: '${groceryInput}'`);
+
+    // Make post request
+    const response = await axios({
+      method: 'post',
+      url: '/api',
+      headers: { 'Content-Type': 'application/json' },
+      data: JSON.stringify({ input: groceryInput }),
+    });
+    const { data }: { data: ResponseData } = response;
+    console.log(data);
+
+    // Assign output data to state
+    setGroceryOutput(data.output);
+  };
+
+  return (
+    <>
+      <h2>Input</h2>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group controlId="grocery-list">
+          <Form.Control
+            as="textarea"
+            rows={10}
+            placeholder="Enter or paste your grocery list items here"
+            value={groceryInput}
+            onChange={(event) => setGroceryInput(event.currentTarget.value)}
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </>
+  );
+};
+
+export default GroceryInput;
--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,14 +1,6 @@
 import React, { useState } from 'react';
-import axios from 'axios';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
 import ListGroup from 'react-bootstrap/ListGroup';
-
-interface ResponseData {
-  message: string;
-  input: string;
-  output: string[];
-}
+import GroceryInput from './GroceryInput';
 
 const Home: React.FC = () => {
   const styles = {
@@ -17,49 +9,12 @@ const Home: React.FC = () => {
     },
   };
 
-  const [groceryInput, setGroceryInput] = useState('');
   const [groceryOutput, setGroceryOutput] = useState<string[]>([]);
-
-  const handleSubmit = async (
-    event: React.FormEvent<HTMLElement>
-  ): Promise<void> => {
-    // Prevent page refresh
-    event.preventDefault();
-
-    console.log(`Form submitted with value: '${groceryInput}'`);
-
-    // Make post request
-    const response = await axios({
-      method: 'post',
-      url: '/api',
-      headers: { 'Content-Type': 'application/json' },
-      data: JSON.stringify({ input: groceryInput }),
-    });
-    const { data }: { data: ResponseData } = response;
-    console.log(data);
-
-    // Assign output data to state
-    setGroceryOutput(data.output);
-  };
 
   return (
     <div className="container" style={styles.topComponent}>
       <h1>Groceries</h1>
-      <h2>Input</h2>
-      <Form onSubmit={handleSubmit}>
-        <Form.Group controlId="grocery-list">
-          <Form.Control
-            as="textarea"
-            rows={10}
-            placeholder="Enter or paste your grocery list items here"
-            value={groceryInput}
-            onChange={(event) => setGroceryInput(event.currentTarget.value)}
-          />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
+      <GroceryInput setGroceryOutput={setGroceryOutput} />
       <h2>Output</h2>
       <ListGroup variant="flush">
         {groceryOutput.map((item) => (
