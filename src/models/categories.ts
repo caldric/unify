ea42c896aa4109d7d9a1fc45a83b8d64bbc8f51f
caldric <cvaldric@gmail.com>
2020-08-17 12:55:09 -0400

    Add categories

--- src/models/categories.ts
+++ src/models/categories.ts
@@ -4,8 +4,62 @@ interface Category {
 }
 
 const categories: Category[] = [
-  { item: 'chicken', section: 'meat' },
-  { item: 'carrot', section: 'produce' },
+  {
+    item: 'chicken',
+    section: 'meat',
+  },
+  {
+    item: 'carrot',
+    section: 'produce',
+  },
+  {
+    item: 'zucchini',
+    section: 'produce',
+  },
+  {
+    item: 'olive oil',
+    section: 'oil',
+  },
+  {
+    item: 'sausage',
+    section: 'meat',
+  },
+  {
+    item: 'red pepper flakes',
+    section: 'condiments',
+  },
+  {
+    item: 'salt',
+    section: 'condiments',
+  },
+  {
+    item: 'pepper',
+    section: 'condiments',
+  },
+  {
+    item: 'onion',
+    section: 'produce',
+  },
+  {
+    item: 'tomato',
+    section: 'produce',
+  },
+  {
+    item: 'bread',
+    section: 'bread',
+  },
+  {
+    item: 'cheese',
+    section: 'dairy',
+  },
+  {
+    item: 'basil',
+    section: 'produce',
+  },
+  {
+    item: 'garlic',
+    section: 'produce',
+  },
 ];
 
 export { categories };
