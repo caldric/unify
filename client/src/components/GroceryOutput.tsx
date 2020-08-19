6f8a93c8a54b934d42c20877753e381d2d55b4d8
caldric <cvaldric@gmail.com>
2020-08-19 00:25:09 -0400

    Add clear shopping list button

--- client/src/components/GroceryOutput.tsx
+++ client/src/components/GroceryOutput.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import './GroceryOutput.css';
+import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 // import ListGroup from 'react-bootstrap/ListGroup';
 
@@ -12,6 +14,9 @@ const GroceryOutput: React.FC<Props> = ({ groceryOutput }) => {
   return (
     <Container className="top-element">
       <h2>Shopping List</h2>
+      <Button id="delete-shopping-list" variant="danger">
+        Clear Shopping List
+      </Button>
       {groceryOutput.map((group) => (
         <div className="grocery-section-output" key={group.section}>
           <h3>{group.section}</h3>
