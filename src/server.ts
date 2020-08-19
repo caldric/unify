f7c959353838d32227a827acadb9e2a82ee359cc
caldric <cvaldric@gmail.com>
2020-08-19 01:23:34 -0400

    Remove production conditional in server.ts

--- src/server.ts
+++ src/server.ts
@@ -36,9 +36,6 @@ mongoose.connection.once('open', () => {
 // Middleware
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
-}
 
 // Routes
 app.use('/api', apiRouter);
